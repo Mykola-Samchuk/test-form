c95390eda63d8d7f50aac3ca2830fa85254c67b7
Kolya Samchuk <mykola.samchuk@gmail.com>
2022-10-15 13:31:38 +0300

    add link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.scss";
-import {Route, Routes } from "react-router-dom";
+import {Route, Routes, Link } from "react-router-dom";
 import Form from "./component/Form/Form";
 import User from "./component/User/User";
 
 function App() {
   return (
     <div className="App">
-      {/* <Form /> */}
+      <Link to={"/"}></Link>
+
       <Routes basename="/form">
         <Route path="/" element={<Form />} />
         <Route path="/user" element={<User />} />
